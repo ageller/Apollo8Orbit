87c955e336bacf240d420cfa52d538b56ff233d9
Aaron Geller <ageller@williamson.local>
2018-12-03 11:49:01 -0600

    Earthrise location, and into Uniview

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Apollo8Orbit
 A python code to calculate orbital elements, using Apollo 8 Moon orbits as an example
+
+The Earthrise photo was taken on December 24, 1968, 16:00 UTC.
---
+++ Uniview3/data/shader.fs
@@ -0,0 +1,19 @@
+uniform float uv_fade;
+
+in vec2 texcoord;
+in vec3 color;
+
+out vec4 fragColor;
+
+void main()
+{
+	fragColor = vec4(color, 1.);
+	fragColor.a *= uv_fade;
+	vec2 fromCenter = texcoord * 2 - vec2(1);
+	float dist = dot(fromCenter, fromCenter);
+	if (dist > 1){
+		discard;
+	}
+	//fragColor.a *= exp(-0.5*dist/0.1);
+
+}
